1f27ca594a0c83b2de96ae48c8012c586d9cea1d
= <=>
2023-03-19 10:08:59 +0200

    Create permissions table

---
+++ app/Models/Permissions.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permissions extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'view_admin_dashboard',
+        'administer_users',
+    ];
+}
