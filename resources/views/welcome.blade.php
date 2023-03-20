05a8db351b7c0b2c7f73a7b6f117106ee464662b
= <=>
2023-03-20 12:14:45 +0200

    Add the table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -200,7 +200,83 @@
                 </button>
             </div>
         </div>
-        </main>
+
+ <!-- new template -->
+<div class="flex flex-col p-8">
+  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="shadow overflow-hidden">
+        <table class="min-w-full">
+          <thead class="">
+            <tr>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                First Name
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                Last Name
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                Email Adress
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                Role
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                Member Since
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-zinc-600 divide-y-8 divide-zinc-900">
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="text-sm text-white">
+                      jane
+                  </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-sm text-white">Doe</div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-white">
+                  jane.cooper@example.com
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-white">
+                Admin
+              </td>
+              <td class="px-6 py-4">
+                <a href="#" class="text-white">21 sep 2021</a>
+              </td>
+            </tr>
+            <!-- second raw -->
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="text-sm text-white">
+                       Thanyani
+                  </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-sm text-white">Khalishi</div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-white">
+                  khalishisimon@gmail.com
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-white">
+                User
+              </td>
+              <td class="px-6 py-4">
+                <a href="#" class="text-white">21 sep 2021</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+</main>
     </div>
     </div>
     </body>
