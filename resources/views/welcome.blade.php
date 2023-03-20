4017ea927536a1d01caec7c420681b44d414a45f
= <=>
2023-03-20 16:00:47 +0200

    Update search form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -183,7 +183,7 @@
         <div class="pl-8 ml-1">
         <label for="search" class="block text-sm font-medium text-white">User name</label>
             <div class="mt-1 relative flex items-center">
-                <input type="text" name="search" id="search" value="Search for users" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block pr-12 sm:text-sm border-gray-300 bg-zinc-600 h-6 w-44 text-gray-400 pl-4">
+                <input type="text" name="search" id="search" placeholder="Search for users" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block pr-12 sm:text-sm border-gray-300 bg-zinc-600 h-6 w-44 text-gray-400 pl-4">
                 <button type="button" class="pl-1">
                    <svg xmlns="http://www.w3.org/2000/svg" 
                         fill="none" 
