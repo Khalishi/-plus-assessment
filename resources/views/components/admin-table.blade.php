c9d35173cea9ae52e818ec89cf5ffdca1cce9ef5
= <=>
2023-03-27 12:42:48 +0200

    extract table component

--- resources/views/components/admin-table.blade.php
+++ resources/views/components/admin-table.blade.php
@@ -1,61 +1,10 @@
 
- <!-- new template -->
 <div class="flex flex-col p-8">
   <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
       <div class="shadow overflow-hidden">
-        <table class="min-w-full">
-          <thead>
-            <tr>
-             <x-table-titles/>
-            </tr>
-          </thead>
-          <tbody class="bg-zinc-600 divide-y-8 divide-zinc-900">
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                    <div class="text-sm text-white">
-                      jane
-                  </div>
-                </div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-white">Doe</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-white">
-                  jane.cooper@example.com
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-white">
-                Admin
-              </td>
-              <td class="px-6 py-4">
-                <a href="#" class="text-white">21 sep 2021</a>
-              </td>
-            </tr>
-            <!-- second raw -->
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                    <div class="text-sm text-white">
-                       Thanyani
-                  </div>
-                </div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-white">Khalishi</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-white">
-                  khalishisimon@gmail.com
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-white">
-                User
-              </td>
-              <td class="px-6 py-4">
-                <a href="#" class="text-white">21 sep 2021</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+            {{ $slot }}
+         </table>
       </div>
     </div>
   </div>
