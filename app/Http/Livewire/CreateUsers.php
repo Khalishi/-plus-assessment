5bd84199bb2d0b51823600ecb2802176e09901ab
= <=>
2023-03-29 16:59:08 +0200

    submit the form

--- app/Http/Livewire/CreateUsers.php
+++ app/Http/Livewire/CreateUsers.php
@@ -6,8 +6,38 @@ use LivewireUI\Modal\ModalComponent;
 
 class CreateUsers extends ModalComponent
 {
+    public $state = [
+        'first_name' => '',
+        'last_name' => '',
+        'email' => '',
+        'role' => '',
+        'password' => '',
+    ];
+
+    public $firstName = '';
+    public $LastName = '';
+    public $email = '';
+    public $password = '';
+    public $ConfirmPassword = '';
+    public $role = 'User';
+
+    public function createUsers()
+    {
+
+        $this->state = [
+            'first_name' => $this->firstName,
+            'last_name' => $this->LastName,
+            'email' => $this->email,
+            'role' => $this->role,
+            'password' => $this->password,
+        ];
+
+        dd($this->state);
+    }
+
     public function render()
     {
         return view('livewire.create-users');
+
     }
 }
--- resources/views/livewire/create-users.blade.php
+++ resources/views/livewire/create-users.blade.php
@@ -1,10 +1,9 @@
-<x-modal>
+<x-modal form-action="createUsers">
      <x-slot name="title">
           Create Users
     </x-slot>
 
      <x-slot name="content">
-
            <div class="py-2 flex">
                 <label for="FirstName"
                  class="block text-xs font-medium text-white">
@@ -17,37 +16,35 @@
                 </label>
             </div>
             <div class="py-2 flex space-x-4">
-                <input type="text"
-                 name="FirstName" id="FirstName"
+                <input  wire:model="firstName" type="text"
                  class="block border-0 border-b-2 border-bg-zinc-900 p-0 bg-zinc-700 w-40 sm:text-xs" >
 
-                <input type="text"
-                 name="LastName" id="LastName"
+                <input wire:model="LastName" type="text"
                  class="block border-0 border-b-2 border-bg-zinc-900 p-0 bg-zinc-700 w-36 sm:text-xs" >
             </div>
-               <div>
+
+            <div>
                 <label for="email"
                   class="block text-xs font-medium text-white">
                     Email
                   </label>
-                  <input type="email"
-                  name="email" id="email"
+                  <input wire:model="email" type="email"
                   class="block border-0 border-b border-bg-zinc-900 p-0 bg-zinc-700 sm:text-xs w-80 mt-4" >
             </div>
 
                 <div>
-                <label for="role"
-                 class="block text-sm font-medium text-white mt-2">
-                    Role
-                  </label>
-                <select id="role"
-                 name="role"
-                  class="block w-80 py-2 text-base border-b border-bg-zinc-900 bg-zinc-700 focus:outline-none sm:text-xs">
-                  <option>Admin</option>
-                  <option selected>Manger</option>
-                  <option>User</option>
-                </select>
+                    <label for="role"
+                    class="block text-sm font-medium text-white mt-2">
+                        Role
+                    </label>
+                    <select wire:model="role"
+                            class="block w-80 py-2 text-base border-b border-bg-zinc-900 bg-zinc-700 focus:outline-none sm:text-xs">
+                            <option>Admin</option>
+                            <option>Manger</option>
+                            <option>User</option>
+                    </select>
               </div>
+
               <div class="py-2 flex">
                 <label for="password"
                  class="block text-xs font-medium text-white">
@@ -60,19 +57,17 @@
                 </label>
             </div>
             <div class="py-2 flex space-x-4">
-                <input type="password"
-                 name="password" id="password"
+                <input wire:model="password" type="password"
                  class="block border-0 border-b-2 border-bg-zinc-600 p-0 bg-zinc-700 w-40 sm:text-xs" >
 
 
-                <input type="password"
-                 name="ConfirmPassword" id="ConfirmPassword"
+                <input wire:model="ConfirmPassword" type="password"
                  class="block border-0 border-b-2 border-bg-zinc-600 p-0 bg-zinc-700 w-36 sm:text-xs" >
             </div>
     </x-slot>
 
      <x-slot name="buttons">
-            <button type="button">
+            <button type="button" wire:click="createUsers">
                 Create Users
             </button>
     </x-slot>
