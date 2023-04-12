fd7ba37f4e04992729aa8303cc9ecff86b9d3f1a
= <=>
2023-04-12 20:29:46 +0200

    add validation

--- app/Http/Livewire/CreateUsers.php
+++ app/Http/Livewire/CreateUsers.php
@@ -2,37 +2,54 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Validator;
 use LivewireUI\Modal\ModalComponent;
 
 class CreateUsers extends ModalComponent
 {
-    public $state = [
-        'first_name' => '',
-        'last_name' => '',
-        'email' => '',
-        'role' => '',
-        'password' => '',
-    ];
 
-    public $firstName = '';
-    public $LastName = '';
-    public $email = '';
-    public $password = '';
-    public $ConfirmPassword = '';
-    public $role = 'User';
+    public $firstName;
+    public $LastName;
+    public $email;
+    public $password;
+    public $ConfirmPassword;
+    public $role;
 
     public function createUsers()
     {
+        $validatedData = Validator::make(
+            [
+                'firstName' => $this->firstName,
+                'LastName' => $this->LastName,
+                'email' => $this->email,
+                'password' => $this->password,
+                'confirmPassword' => $this->ConfirmPassword,
+            ],
 
-        $this->state = [
-            'first_name' => $this->firstName,
-            'last_name' => $this->LastName,
-            'email' => $this->email,
-            'role' => $this->role,
-            'password' => $this->password,
-        ];
+            [
+                'firstName' => 'required|min:3|max:50',
+                'LastName' => 'required|min:3|max:50',
+                'email' => 'required|email|max:255|unique:users',
+                'password' => 'required|min:8|max:255',
+                'ConfirmPassword' => 'required|same:password',
+                'role' => 'required',
+            ]
+        )->validate();
 
-        dd($this->state);
+        // Save the user to the database
+        $user->save();
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName, [
+            'firstName' => 'required|min:3|max:50',
+            'LastName' => 'required|min:3|max:50',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8|max:255',
+            'ConfirmPassword' => 'required|same:password',
+            'role' => 'required',
+        ]);
     }
 
     public function render()
