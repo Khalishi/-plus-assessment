1eb6a240e28cad3bc41666fc3f1030586abf6de8
= <=>
2023-04-18 13:42:52 +0200

    list users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
+            $table->string('firstName');
+            $table->string('lastName');
             $table->string('email')->unique();
-            $table->string('role')->unique();
+            $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ resources/views/livewire/user-list.blade.php
@@ -0,0 +1,25 @@
+@foreach($users as $user)
+<tbody class="bg-zinc-600 divide-y-8 divide-zinc-900">
+<tr>
+    <td class="px-6 py-4 whitespace-nowrap">
+       <div class="flex items-center">
+          <div class="text-sm text-amber-300 underline">
+                 <a href="/admin/edit">{{ $user->firstName }}</a>
+          </div>
+      </div>
+  </td>
+<td class="px-6 py-4 whitespace-nowrap">
+     <div class="text-sm text-white">{{ $user->lastName }}</div>
+</td>
+<td class="px-6 py-4 whitespace-nowrap text-white">
+      {{ $user->email }}
+ </td>
+<td class="px-6 py-4 whitespace-nowrap text-sm text-white">
+    {{ $user->role }}
+</td>
+<td class="px-6 py-4">
+    <a href="#" class="text-white">{{ $user->created_at }}</a>
+</td>
+</tr>
+</tbody>
+@endforeach
